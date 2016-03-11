26952ffe5e53626eff276d49ea52117eb449c677
SHOOROOP <shooroop@rambler.ru>
2016-03-12 00:39:25 +0300

    Обработка выхода на сервере

--- PHP/HostConnection.php
+++ PHP/HostConnection.php
@@ -21,4 +21,21 @@ class HostConnection
         mysqli_close($connection);
         return $arr;
     }
+
+    public function LogOut($host, $login)
+    {
+        $r1 = setcookie("login", null, -1, '/');
+        $r2 = setcookie("token", null, -1, '/');
+
+        $connection = mysqli_connect($host[0], $host[1], $host[2], $host[3]) or die (json_encode("Error " . mysqli_error($connection)));
+        $connection->query('SET NAMES utf8');
+
+        $query = "UPDATE userstable SET token=0 WHERE login='$login'";
+        $connection->query($query);
+
+        mysqli_close($connection);
+
+
+        return ($r1 & $r2);
+    }
 }
--- PHP/records.php
+++ PHP/records.php
@@ -7,10 +7,14 @@ $host = Host::getLocal();
 $state = new HostConnection();
 $params = json_decode(file_get_contents('php://input'));
 $command = $params->command;
+$login = $params->login;
 
 switch($command){
     case "getRecords":
         $result = $state->getRecords($host);
+        break;
+    case "LogOut":
+        $result = $state->LogOut($host, $login);
         break;
     default:
         $result = "no such option.";
