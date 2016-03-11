24d3696dd9c4e1c8fc237eeec0ced45cea2a44b9
SHOOROOP <shooroop@rambler.ru>
2016-03-12 00:12:14 +0300

    Обработка запроса на вход на сервере

--- PHP/HostConnection.php
+++ PHP/HostConnection.php
@@ -21,4 +21,27 @@ class HostConnection
         mysqli_close($connection);
         return $arr;
     }
+
+    public function LogIn($host, $login, $password)
+    {
+        $token = rand(100000, 999999);
+
+        setcookie("login", $login, time() + 3600 * 24, '/', false, false);
+        setcookie("token", $token, time() + 3600 * 24, '/', false, false);
+
+        $connection = mysqli_connect($host[0], $host[1], $host[2], $host[3]) or die (json_encode("Error " . mysqli_error($connection)));
+        $connection->query('SET NAMES utf8');
+
+        $query = "SELECT * FROM userstable WHERE login='$login' AND password='$password'";
+        $result = $connection->query($query);
+        if ($result->num_rows <= 0) {
+            return false;
+        }
+
+        $query = "UPDATE userstable SET token=$token WHERE login='$login'";
+        $connection->query($query);
+
+        mysqli_close($connection);
+        return true;
+    }
 }
--- PHP/records.php
+++ PHP/records.php
@@ -7,10 +7,16 @@ $host = Host::getLocal();
 $state = new HostConnection();
 $params = json_decode(file_get_contents('php://input'));
 $command = $params->command;
+$login = $params->login;
+$password = $params->password;
+
 
 switch($command){
     case "getRecords":
         $result = $state->getRecords($host);
+        break;
+    case "Login":
+        $result = $state->Login($host, $login, $password);
         break;
     default:
         $result = "no such option.";
