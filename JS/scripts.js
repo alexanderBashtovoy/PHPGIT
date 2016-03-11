27fc4f74925eed202d3ce5971ee5647bd77379d4
SHOOROOP <shooroop@rambler.ru>
2016-03-12 01:16:10 +0300

    Добавление валидации

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,6 +4,37 @@
 var bookApp = angular.module('bookApp',[]);
 
 $(document).ready(function(){
+    $(function () {
 
+        $('#inputForm').validate({
+            highlight: function (element, errorClass) {
+                $(element).addClass("invalidElem");
+            },
+            unhighlight: function (element, errorClass) {
+                $(element).removeClass("invalidElem");
+            },
+            errorElement: "div",
+            errorClass: "errorMsg"
+        });
+
+        $.validator.addClassRules({
+            inputValidation: {
+                required: true,
+            }
+        })
+
+        $('input').addClass("inputValidation").change(function (e) {
+            $('#inputForm').validate().element($(e.target));
+        });
+
+        $('#login').each(function (index, elem) {
+            $(elem).rules("add", {
+                required: true,
+                messages: {
+                    required: "Поле обязательно к заполнению",
+                }
+            });
+        });
+    });
 });
 
