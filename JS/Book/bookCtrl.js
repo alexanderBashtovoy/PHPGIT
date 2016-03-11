690be62a67ffc115049375332c7953d9829b9cbd
SHOOROOP <shooroop@rambler.ru>
2016-03-12 00:32:30 +0300

    Обработка нажатия кнопки добавления сообщения

--- JS/Book/bookCtrl.js
+++ JS/Book/bookCtrl.js
@@ -27,4 +27,38 @@ bookApp.controller('bookController', function ($scope, $log, bookService) {
             //$log.info($scope.records);
         });
     };
+
+    $scope.modalSubmit = function () {
+        switch ($scope.title) {
+            case "Добавить сообщение":
+                bookService.addMessage(function (records) {
+                    if (records != null && records != "") {
+                        $scope.records = records;
+                        $scope.title = "";
+                        $("#myModal").modal('hide');
+                        //location.reload();
+                    }
+                    else {
+                        $scope.error = "Ошибка";
+                    }
+
+                }, $scope.login, $scope.message);
+                location.reload();
+                break;
+        }
+    }
+
+    $scope.onAddMessage = function () {
+        if ($scope.logining) {
+
+        }
+        else {
+            $scope.loginPlaceholder = "Назовите себя";
+        }
+
+        $scope.title = "Добавить сообщение";
+        $scope.submit = "Отправить";
+        $scope.show = true;
+        $("#myModal").modal('show');
+    }
 });
