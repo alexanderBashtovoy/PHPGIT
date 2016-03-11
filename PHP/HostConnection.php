f4c301f7c2566c8ebfb01bf1cb335428c5b03647
SHOOROOP <shooroop@rambler.ru>
2016-03-11 23:13:59 +0300

    Создание и вызов на сервере метода получения сообщений из базы

--- PHP/HostConnection.php
+++ PHP/HostConnection.php
@@ -2,5 +2,23 @@
 
 class HostConnection
 {
+    public function getRecords($host)
+    {
+        //error_reporting(0);
+        $connection = mysqli_connect($host[0], $host[1], $host[2], $host[3]) or die (json_encode("Error " . mysqli_error($connection)));
+        $connection->query('SET NAMES utf8');
 
+        $arr = [];
+        $query = "SELECT * FROM recordsTable";
+        $result = $connection->query($query);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $arr[] = $row;
+            }
+        }
+
+        mysqli_close($connection);
+        return $arr;
+    }
 }
--- PHP/records.php
+++ PHP/records.php
@@ -3,3 +3,17 @@ require_once 'host.php';
 require_once 'HostConnection.php';
 
 $host = Host::getLocal();
+
+$state = new HostConnection();
+$params = json_decode(file_get_contents('php://input'));
+$command = $params->command;
+
+switch($command){
+    case "getRecords":
+        $result = $state->getRecords($host);
+        break;
+    default:
+        $result = "no such option.";
+}
+
+echo json_encode($result);
